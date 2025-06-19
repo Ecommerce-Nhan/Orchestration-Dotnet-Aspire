548850201613c02a99ec8f248523de2fb6445b4b
nhan.tran@bowaresolutions.com <Nhan Boware>
2025-06-19 17:28:45 +0700

    Feature: remove auth service and ocelot

--- Orchestration.AppHost/Program.cs
+++ Orchestration.AppHost/Program.cs
@@ -1,9 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redis = builder.AddRedis("redis");  
+var redis = builder.AddRedis("redis");
 
-var apiGateway = builder.AddProject<Projects.APIGateway>("api-gateway");
-var authService = builder.AddProject<Projects.AuthService>("auth-service").WithReference(redis);
 var productService = builder.AddProject<Projects.ProductService_Api>("product-service").WithReference(redis);
 var userService = builder.AddProject<Projects.UserService_Api>("user-service").WithReference(redis);
 
